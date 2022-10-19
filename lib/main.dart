b906804b64ce26c9ec19b125f558d7708c0d2d24
jahidhasanm1 <jahidnsu142@gmail.com>
2022-10-19 18:27:40 +0600

    Initialized Routing in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:sample_flutter_project/routes/app_pages.dart';
 
 
 void main() {
   runApp(
-    const GetMaterialApp(
+     GetMaterialApp(
       title: "Application",
+       debugShowCheckedModeBanner: false,
+       initialRoute: AppPages.INITIAL,
+       getPages: AppPages.routes,
+       defaultTransition: Transition.topLevel,
     ),
   );
 }
